0baa88d738a323345428c572e631161443df9668
ddolbang96 <ddolbang96@gmail.com>
2021-04-07 00:25:37 +0900

    message edit

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ print("Hello World")
 #Adding new item in Hotfix
 print("Hello World 2")
 
-#Adding new item in Hotfix
+#Adding new item in Master
 print("Hello World 3")
 
 #Adding new item in Hotfix
 print("Hello World 4")
 
-#Adding new item in Hotfix
+#Adding new item in Master
 print("Hello World 5")
